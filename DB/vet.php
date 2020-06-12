ecaf64358dc5377d797bd46ba2b89b5eec1f4afe
xhereshx <rasticamichal@email.cz>
2020-06-12 16:18:07 +0200

    looptwo

--- DB/vet.php
+++ DB/vet.php
@@ -17,12 +17,18 @@ $data = json_decode(file_get_contents('clients.json'), true);
 
 
 //var_dump($data[0]["surname"]);
-var_dump($data[0]);
-
+//var_dump($data[0]);
+var_dump($data);
+echo gettype($data[0]["first_name"]);
 // var_dump($data[0]["first_name"]["surname"]);
 
-DB::insert("INSERT into `owner` (`firstname`, `surename`, `homeaddress`, `emailaddress`, `telephonenumber`, `info`) 
-VALUES ($data[0]['firstname'], $data[0]['surename'], 'xxx' , 'emailaddress', 'telephonenumber','info')");
+// DB::insert("INSERT into `owner` (`firstname`, `surname`, `homeaddress`, `emailaddress`, `telephonenumber`, `info`) VALUES ('" . $data[$i]['first_name'] . " ' , ' " . $data[$i]['surname'] . " ', 'xxx', 'emailaddress', 'telephonenumber', 'info')");
+
+for($i = 0; $i < 2000; $i++){
+     DB::insert("INSERT into `owner` (`firstname`, `surname`, `homeaddress`, `emailaddress`, `telephonenumber`, `info`) VALUES ('" . $data[$i]['first_name'] . " ' , ' " . $data[$i]['surname'] . " ', 'xxx', 'emailaddress', 'telephonenumber', 'info')");
+}
+
+//DB::insert("INSER")
 
 // $nr_of_inserted = DB::insert('INSERT into `users` (`id`, `name`) VALUES (1, "Dayle")');
 
