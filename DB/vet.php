0ade88658c5939ff92217fff4573af303268059f
hazminfirdaus <cmhazminfirdaus@gmail.com>
2020-06-12 18:44:17 +0800

    added DB

---
+++ DB/vet.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'DB.php';
+require_once 'DB_functions.php';
+
+$success = connect('localhost', 'laravel-vet', 'root', 'rootroot');
+
+// $data = json_decode(file_get_contents('clients.json'), true);
+
+// var_dump($data);
+
+
+
+
