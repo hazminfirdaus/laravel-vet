eeaeb72711aa4389d7eb6da18feeb955eb63ecb4
xhereshx <rasticamichal@email.cz>
2020-06-12 16:16:23 +0200

    loop

--- DB/vet.php
+++ DB/vet.php
@@ -16,13 +16,18 @@ DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 $data = json_decode(file_get_contents('clients.json'), true);
 
 
-// var_dump($data[0]["surname"]);
+//var_dump($data[0]["surname"]);
+var_dump($data[0]);
 
 // var_dump($data[0]["first_name"]["surname"]);
 
-DB::insert("INSERT into `owner` (`firstname`, `surename`, `homeaddress`, `emailaddress`, `telephonenumber`, `info`) VALUES ('firstname', 'surename', 'homeaddress', 'emailaddress', 'telephonenumber', 'info')");
+DB::insert("INSERT into `owner` (`firstname`, `surename`, `homeaddress`, `emailaddress`, `telephonenumber`, `info`) 
+VALUES ($data[0]['firstname'], $data[0]['surename'], 'xxx' , 'emailaddress', 'telephonenumber','info')");
 
 // $nr_of_inserted = DB::insert('INSERT into `users` (`id`, `name`) VALUES (1, "Dayle")');
+
+//$data[0]["first_name"]
+
 
 
 
