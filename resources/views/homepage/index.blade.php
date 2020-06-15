23b27a0aece85a7f19805a39c3c2efe2a0452f39
hazminfirdaus <cmhazminfirdaus@gmail.com>
2020-06-15 11:23:59 +0800

    changed colour

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -3,9 +3,11 @@
 ])
 
 @section('content')
-    <img class="logo" src="img/logo.jpg" alt="logo">
-    <div class="title">
-      <h1>St. Hector's</h1>
-      <h2>Veterinary Clinic</h2>
+    <div class="title--container">
+        <img class="logo" src="img/logo.jpg" alt="logo">
+        <div class="title">
+        <h1>St. Hector's</h1>
+        <h2>Veterinary Clinic</h2>
+        </div>
     </div>
 @endsection
